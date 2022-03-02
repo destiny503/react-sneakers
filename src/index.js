1e13ce76f8b9a00bfcc30d4c1986beb57c008ff0
tsartsartsar <a.rahmatullin1999@gmail.com>
2022-03-02 19:47:22 +0400

    super mega fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom';
 
 import './index.scss';
@@ -9,9 +9,9 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
