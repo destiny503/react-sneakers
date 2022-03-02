7bd71a2a63b96722865e1895134e7ba9a0adeb37
tsartsartsar <a.rahmatullin1999@gmail.com>
2022-03-02 21:12:44 +0400

    super mega fix1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom';
 
 import './index.scss';
@@ -9,9 +9,10 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    {/* basename чтобы гитхаб пейджес работал */}
+    <BrowserRouter basename="/react-sneakers">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
